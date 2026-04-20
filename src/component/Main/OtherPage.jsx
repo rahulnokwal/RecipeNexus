209409858cec770364a3646c26308a17df29dd2e
Rahul Nokwal <nokwalrahul09@gmail.com>
2026-04-20 10:50:51 +0530

    fixed scroll issue

--- src/component/Main/OtherPage.jsx
+++ src/component/Main/OtherPage.jsx
@@ -1,9 +1,21 @@
 import { Outlet } from "react-router-dom";
-
+import { useRef, useEffect } from "react";
+import useAppContext from "../../context/useRecipeContext";
 const OtherPage = () => {
+  const { clickedRecipe } = useAppContext();
+  const scrollContainerRef = useRef(null);
+  useEffect(() => {
+    if (clickedRecipe && scrollContainerRef.current) {
+      scrollContainerRef.current.scrollTo({
+        top: 0,
+        behavior: "auto",
+      });
+    }
+  }, [clickedRecipe]);
   return (
     <div
       id="scrollbar"
+      ref={scrollContainerRef}
       className="relative top-15 left-0 h-[calc(100dvh-124px)] sm:h-[calc(100dvh-140px)] px-4 my-2 overflow-auto"
     >
       <Outlet />
